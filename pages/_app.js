a08cdac162b7766052c0937491f73cb83e400164
abdulwaqar844 <abdulwaqar844@gmail.com>
2022-10-20 07:55:42 +0500

    animate_css configured

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
-import "bootstrap/dist/css/bootstrap.css";
 import "../styles/styles.scss";
 import Layout from "../components/layouts";
 import { useEffect } from "react";
-
+import 'animate.css';
 function MyApp({ Component, pageProps }) {
   useEffect(()=>{
     import("bootstrap/dist/js/bootstrap");
@@ -13,6 +12,4 @@ function MyApp({ Component, pageProps }) {
     </Layout>
   );
 }
-
 export default MyApp;
-// styles/styles.scss
