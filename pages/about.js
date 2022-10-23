3a373a57af0952e584c44ef3e701278db9a0f36e
abdulwaqar844 <abdulwaqar844@gmail.com>
2022-10-23 12:21:02 +0500

    updated about page

--- pages/about.js
+++ pages/about.js
@@ -23,6 +23,7 @@ export default function about({ data }) {
   );
 }
 export async function getStaticProps() {
+  
   let res = await axios.get(
     "https://raw.githubusercontent.com/abdulwaqar844/TS/main/README.md"
   );
